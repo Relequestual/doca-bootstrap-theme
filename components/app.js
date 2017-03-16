e0098c5f4020a7a2e4425577121f1c2b9d8223b9
Ben Hutton <bh7@sanger.ac.uk>
2017-03-16 16:07:40 +0000

    Added ability to add introduction content as markdown. Swapped out to `marked` module, and configured to render bootstrap tables and headers with anchors.

--- components/app.js
+++ components/app.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ImmutablePropTypes = require('react-immutable-proptypes');
-const MarkdownPreview = require('react-marked-markdown').MarkdownPreview;
+const marked = require('marked');
 const Component = require('react-pure-render/component');
 const Sidebar = require('./sidebar');
 const Schema = require('./schema');
@@ -15,6 +15,24 @@ class App extends Component {
   render() {
     const { schemas, config } = this.props;
 
+    const renderer = new marked.Renderer();
+
+    renderer.table = (header, body) =>
+      `<table class="table">
+        <thead>${header}</thead>
+        <tbody>${body}</tbody>
+      </table>`;
+
+    renderer.heading = (text, level) => {
+      const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+      return `<h${level}>
+        <a name="${escapedText}" class="anchor" href="#${escapedText}">
+          <span class="header-link"></span>
+        </a>
+      ${text}</h${level}>`;
+    };
+
     return (
       <div className="container-fluid" id="wrapper">
         <div className="row">
@@ -23,9 +41,11 @@ class App extends Component {
           </div>
           <div className="col-sm-9 col-lg-10" id="page-content-wrapper">
             <h1>{config.title}</h1>
-            {config.intro && <div>
-              <MarkdownPreview value={config.intro} />
+            {config.intro && <div
+              dangerouslySetInnerHTML={{ __html: marked(config.intro, { renderer }) }}
+            >
             </div>}
+            <h1>Objects and Endpoints</h1>
             {schemas
               .filter(schema => !schema.get('hidden'))
               .valueSeq()
