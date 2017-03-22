ac446a1735a683af8bb9e39fe1430dca85abce48
Ben Hutton <bh7@sanger.ac.uk>
2017-03-22 11:49:08 +0000

    If intro text is given, construct the navigation content for the sidebar using the headers from the markdown text.

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,7 +1,10 @@
 const React = require('react');
 const Component = require('react-pure-render/component');
 const ImmutablePropTypes = require('react-immutable-proptypes');
+const { List } = require('immutable');
 const Nav = require('react-bootstrap/lib/Nav');
+const marked = require('marked');
+
 const Navbar = require('react-bootstrap/lib/Navbar');
 const NavDropdown = require('react-bootstrap/lib/NavDropdown');
 const MenuItem = require('react-bootstrap/lib/MenuItem');
@@ -12,6 +15,34 @@ class Sidebar extends Component {
     schemas: ImmutablePropTypes.list.isRequired,
     config: React.PropTypes.object,
   };
+
+  renderNavTitle = (text) => {
+    const titles = new List(marked.lexer(text))
+      .filter(element => element.type === 'heading')
+      .map(title => {
+        const newTitle = title;
+        newTitle.slug = title.text.toLowerCase().replace(/[^\w]+/g, '-');
+        return newTitle;
+      });
+
+    return titles.filter(title => title.depth === 1).map((title) =>
+      <NavDropdown title={title.text} id="nav-dropdown" key={`${title.slug}`}>
+          {
+            (titles
+            .skipWhile(testTitle => testTitle !== title)
+            .skipUntil(testTitle => testTitle !== title)
+            .takeUntil(subTitle => subTitle.depth === 1))
+            .map((subTitle) =>
+              <MenuItem
+                href={`#${subTitle.slug}`}
+                key={`${title.slug}-${subTitle.slug}`}
+              >{subTitle.text}</MenuItem>
+            )
+          }
+      </NavDropdown>
+    );
+  };
+
 
   render() {
     const { schemas, config } = this.props;
@@ -25,15 +56,21 @@ class Sidebar extends Component {
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-            <Nav>
-              {schemas.filter(schema => !schema.get('hidden')).valueSeq().map(schema =>
-                <NavDropdown title={schema.get('title')} id="nav-dropdown" key={`${schema.get('title')}`} >
-                    {schema.get('links').valueSeq().map(link =>
-                    <MenuItem href={`#${link.get('html_id')}`} key={`${schema.get('title')}-${link.get('title')}`} >{link.get('title')}</MenuItem>
-                    )}
-                </NavDropdown>
-              )}
-           </Nav>
+          <Nav>
+            {config.intro && this.renderNavTitle(config.intro)}
+            {schemas.filter(schema => !schema.get('hidden')).valueSeq().map(schema =>
+              <NavDropdown
+                title={schema.get('title')}
+                id="nav-dropdown"
+                key={schema.get('title')}
+              >
+                <MenuItem href={`#${schema.get('html_id')}`}>Object Definition</MenuItem>
+                {schema.get('links').valueSeq().map(link =>
+                  <MenuItem href={`#${link.get('html_id')}`} key={`${schema.get('title')}-${link.get('title')}`} >{link.get('title')}</MenuItem>
+                )}
+              </NavDropdown>
+            )}
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
     );
